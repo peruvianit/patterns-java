675db6ce2da7eb7d832ef91c719ca717f14b19bf
Sergio Arellanom <sergioarellanodiaz@gmail.com>
2025-08-25 15:18:27 +0200

    Pattern strutturali package-info.java

---
+++ src/it/peruvianit/pattern/strutture/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * <h1>Package it.peruvianit.pattern.strutture  </h1>
+ * I pattern strutturali si occupano della composizione delle classi e degli oggetti per formare strutture più grandi.
+ * Questi pattern semplificano la progettazione, identificando modi per definire le relazioni tra le entità.
+ *
+ *  - Adapter: permette a classi con interfacce incompatibili di collaborare.
+ *  - Decorator: aggiunge nuove funzionalità a un oggetto dinamicamente, avvolgendolo.
+ *  - Composite: compone oggetti in strutture ad albero per rappresentare gerarchie parte-intero. Permette ai client di
+ *    trattare oggetti individuali e composizioni di oggetti in modo uniforme.
+ *  - Facade: fornisce un'interfaccia semplificata a un sottosistema complesso.
+ *  - Bridge: separa un'astrazione dalla sua implementazione, permettendo a entrambe di variare indipendentemente.
+ *  - Flyweight: riduce l'uso della memoria condividendo lo stato tra più oggetti.
+ *
+ * @since 1.0
+ * @author Sergio Arellano
+ */
+package it.peruvianit.pattern.strutture;
